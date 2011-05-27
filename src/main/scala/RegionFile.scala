95206b953812711b6050ab119ba01e49d67748ea
Bertrand Chazot <chazot@enst.fr>
2011-05-27 18:25:35 +0200

    Add Java classes that read region files Add a Scala object wrapping the Java

---
+++ src/main/scala/RegionFile.scala
@@ -0,0 +1,12 @@
+
+
+object Chunk {
+  import com.mojang.RegionFileCache
+  import java.io.File
+  import java.io.DataInputStream
+
+  def apply(path: String, x: Int, z: Int) =
+    RegionFileCache.getRegionFile(new File(path), x, z).getChunkDataInputStream(x & 31 , x & 31 )
+  }
+
+
