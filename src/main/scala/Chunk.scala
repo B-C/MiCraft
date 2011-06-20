0fc37319544f3e2ad712654531da1b0819f4a271
Bertrand Chazot <chazot@enst.fr>
2011-06-21 01:05:20 +0200

    Add outlines for somme classes

---
+++ src/main/scala/Chunk.scala
@@ -0,0 +1,7 @@
+
+
+class Chunk(tag: Tag){
+  
+
+
+}
---
+++ src/main/scala/Level.scala
@@ -0,0 +1,6 @@
+
+case class Level(path: String){
+  
+}
+
+// RegionFile("/home/wrath/Programmation/PLNC/scala/minecraft/src/main/resources",0,0)
