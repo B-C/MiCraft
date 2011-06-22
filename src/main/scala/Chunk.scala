2d05e58091e9f37f648ca5079561857a4f3a8cc2
Bertrand Chazot <chazot@enst.fr>
2011-06-22 22:54:21 +0200

    Draw only blocks which ara visible

--- src/main/scala/Chunk.scala
+++ src/main/scala/Chunk.scala
@@ -1,4 +1,3 @@
-
 case class Chunk(tag: Tag){
   val data = tag.asInstanceOf[CompoundTag]
   val levelTag = data.value.head.asInstanceOf[CompoundTag]
@@ -8,24 +7,50 @@ case class Chunk(tag: Tag){
   val blockData = levelTag.get("Blocks").get.asInstanceOf[ByteArrayTag]
   // val mapData = levelTag.get("Data").get.asInstanceOf[ByteArrayTag]
 
-  def getBlock(x: Int, y: Int, z: Int): Byte = 
-    blockData.value(y + (z * 128) + (x * 128 * 16))
+  def getBlock(x: Int, y: Int, z: Int): Byte =
+    blockData.value(y + z*128 + x*128*16)
 }
 
 class ChunkDrawable(tag: Tag) extends Chunk(tag) {
-  
+
+  private val nbBlocks = 16*128*16
+
+  val drawable = Array.ofDim[Boolean](nbBlocks)
+
+  for(x <- 0 until 16)
+    for(z <- 0 until 16)
+      for(y <- 0 until 128)
+	drawable(y + z*128 + x*128 *16)=isDrawable(x,y,z)
+
+  private def isDrawable(x: Int, y:Int, z:Int): Boolean = {
+    if( y==127 || y==0 || x==15 || x==0 || z==15 || z==0)
+      true
+    else
+      getBlock(x+1,y,z)==0 ||
+      getBlock(x-1,y,z)==0 ||
+      getBlock(x,y+1,z)==0 ||
+      getBlock(x,y-1,z)==0 ||
+      getBlock(x,y,z+1)==0 ||
+      getBlock(x,y,z+1)==0
+  }
+
   def draw(parent: processing.core.PApplet) = {
-    for(x <- 0 until 16)
-      for(z <- 0 until 16)
+    for(x <- 0 until 16){
+      var offsetX = x*128*16
+      for(z <- 0 until 16){
+	var counter = offsetX + z*128
 	for(y <- 0 until 128){
-	  var id = getBlock(x,y,z)
-	  if (id>=1){
+	  var id = blockData.value(counter)
+	  if (id>=1 && drawable(counter)){
 	    parent.pushMatrix
 	    parent.translate(x * Block.SIZE, z * Block.SIZE, -y * Block.SIZE)
 	    parent.scale(Block.SIZE/2)
 	    Block.draw(id,parent)
 	    parent.popMatrix
 	  }
+	  counter+=1
 	}
+      }
+    }
   }
 }
