5b30be7205334f11eddc560ed0c102e402d99e24
Bertrand Chazot <chazot@enst.fr>
2011-06-22 23:35:50 +0200

    Fn show one type of block

--- src/main/scala/MiCraft.scala
+++ src/main/scala/MiCraft.scala
@@ -1,8 +1,10 @@
 import processing.core._
 
 object MiCraft extends AppletWithCamera {
-
-  var chunk = new ChunkDrawable(Tag(RegionFile("src/main/resources",0,5)), List(16))
+  
+  
+  var visibleBlocks: List[Int] = List(8,910,11) //lava water visible
+  var chunk = new ChunkDrawable(Tag(RegionFile("src/main/resources",0,5)), visibleBlocks)
 
 
 
@@ -34,8 +36,33 @@ object MiCraft extends AppletWithCamera {
     chunk.draw(this)
   }
 
+  def listUpdate(id:Int) = {
+    if(visibleBlocks.exists(_==id))
+      visibleBlocks=visibleBlocks.filterNot(_==id)
+    else
+      visibleBlocks=id::visibleBlocks
+
+    chunk.visibleBlocks(visibleBlocks)
+  }
+       
+
   override def keyPressed(): Unit = {
+    import java.awt.event.KeyEvent
     super.keyPressed()
+    if (key == PConstants.CODED)
+      keyCode match{
+    	case KeyEvent.VK_F1 => listUpdate(16) //coal
+    	case KeyEvent.VK_F2 => listUpdate(15) //iron
+    	case KeyEvent.VK_F3 => listUpdate(14) //gold
+    	case KeyEvent.VK_F4 => listUpdate(56) //diamond
+    	case KeyEvent.VK_F5 => listUpdate(21) //lapis lazuli
+    	case KeyEvent.VK_F6 => listUpdate(73) //redstone
+    	case KeyEvent.VK_F7 => listUpdate(49) //obsidian
+    	case KeyEvent.VK_F8 => listUpdate(82) //clay
+    	case KeyEvent.VK_F9 => listUpdate(52) //spawner
+    	case KeyEvent.VK_F10 => listUpdate(86) //pumpkin
+    	case _ => ()
+      }
   }
 
   def main(args: Array[String]): Unit = {
