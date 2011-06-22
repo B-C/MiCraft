50cb4d94e7088c898dcc5b8dadbdb4eb34384f21
Bertrand Chazot <chazot@enst.fr>
2011-06-22 13:15:58 +0200

    Add README

--- src/main/scala/Chunk.scala
+++ src/main/scala/Chunk.scala
@@ -1,5 +1,4 @@
 
-
 case class Chunk(tag: Tag){
   val data = tag.asInstanceOf[CompoundTag]
   val levelTag = data.value.head.asInstanceOf[CompoundTag]
@@ -8,4 +7,23 @@ case class Chunk(tag: Tag){
   val entities = levelTag.get("Entities").get.asInstanceOf[ListTag]
   val blockData = levelTag.get("Blocks").get.asInstanceOf[ByteArrayTag]
   val mapData = levelTag.get("Data").get.asInstanceOf[ByteArrayTag]
+
+  def getBlock(x: Int, y: Int, z: Int): Byte = 
+    blockData.value(y + (z * 128) + (x * 128 * 16))
+  
+  
+
 }
+
+		for(int x=0;x<16;x++) {
+			int xOff = (x * 128 * 16);
+			for(int z=0;z<16;z++) {
+				int zOff = (z * 128);
+				int blockOffset = zOff + xOff-1;
+				for(int y=0;y<128;y++) {
+					blockOffset++;
+					byte t = blockData.value[blockOffset];
+					
+					if(t < 1) {
+						continue;
+					}
